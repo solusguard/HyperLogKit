a263dac5ae8c2a8e5b4b70c25f6170c817448eb0
tobias <tobias@thecodedev.de>
2020-06-16 11:14:42 +0200

    Set swift-tool-version to 5.1

--- Package.swift
+++ Package.swift
@@ -1,3 +1,5 @@
+// swift-tools-version:5.1
+
 // Package.swift
 //
 // Copyright (c) 2015 - 2016, Justin Pawela & The LogKit Project
@@ -14,8 +16,6 @@
 // WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 // ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
-// swift-tools-version:5.1
 
 import PackageDescription
 

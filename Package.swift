6746013f4028b50d1523a33aa43daa21aad22bf0
tobias <tobias@thecodedev.de>
2020-06-16 11:18:13 +0200

    Reorder SwiftPM entries

--- Package.swift
+++ Package.swift
@@ -21,16 +21,16 @@ import PackageDescription
 
 let package = Package(
         name: "HyperLogKit",
-        products: [
-            .library(
-                    name: "HyperLogKit",
-                    targets: ["HyperLogKit"]),
-        ],
         platforms: [
             .iOS(.v10),
             .macOS(.v10_10),
             .tvOS(.v9),
             .watchOS(.v2),
+        ],
+        products: [
+            .library(
+                    name: "HyperLogKit",
+                    targets: ["HyperLogKit"]),
         ],
         targets: [
             .target(
